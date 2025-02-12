fa88bd199166f8761f557c3925499cb82cceeaf4
Ashesh <ashesh276@gmail.com>
2025-02-12 12:35:17 +0100

    config update for noisy HT-LIF data

--- data/HT_LIF_rawdata.py
+++ data/HT_LIF_rawdata.py
@@ -5,4 +5,5 @@ def load_HT_LIF_data(directory):
     fnames = [os.path.join(directory, f) for f in os.listdir(directory)]
     assert len(fnames) == 1
     fpath  = os.path.join(directory, fnames[0])
+    print('Loading data from', fpath)
     return imread(fpath, plugin='tifffile')

12321bbec5cc63366fd968a5cb190d6db67a4024
Ashesh <ashesh276@gmail.com>
2025-02-27 07:20:40 +0100

    updated scripts

--- notebooks/htt24_indi.sh
+++ notebooks/htt24_indi.sh
@@ -1,15 +1,15 @@
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.1
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.2
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.3
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.4
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.1 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.2 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.3 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.4 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/15 --mixing_t_ood=0.5 --use_hardcoded_time_for_inference=0.5
 
 
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.1
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.2
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.3
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.4
-python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.1 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.2 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.3 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.4 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.5 --use_hardcoded_time_for_inference=0.5
 
 
 
@@ -32,3 +32,7 @@ python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.
 python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.3 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/HT_T24-UnetClassifier-l2/3
 python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.4 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/HT_T24-UnetClassifier-l2/3
 python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/14 --mixing_t_ood=0.5 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/HT_T24-UnetClassifier-l2/3
+
+
+# real input
+python /home/ashesh.ashesh/code/DiffSplitting/notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/15 --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndiRealInput.ipynb  --use_hardcoded_time_for_inference=0.5
---
+++ notebooks/inputVsTarget.sh
@@ -0,0 +1,16 @@
+python notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/14 --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndiRealInput.ipynb --ckpt_time_predictor=2502/HT_T24-UnetClassifier-l2/3 --infer_time=true --use_aggregated_inferred_time=true --enable_real_input=true
+
+
+python notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/14 --mmse_count=1 --mixing_t_ood=0.1 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/14 --mmse_count=1 --mixing_t_ood=0.2 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/14 --mmse_count=1 --mixing_t_ood=0.3 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/14 --mmse_count=1 --mixing_t_ood=0.4 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/HT_T24-joint_indi-l1/14 --mmse_count=1 --mixing_t_ood=0.5 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+
+
+python notebooks/evaluate_notebook.py --ckpt=2502/BioSR-joint_indi-l1/5 --mmse_count=1 --mixing_t_ood=0.1 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/BioSR-joint_indi-l1/5 --mmse_count=1 --mixing_t_ood=0.2 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/BioSR-joint_indi-l1/5 --mmse_count=1 --mixing_t_ood=0.3 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/BioSR-joint_indi-l1/5 --mmse_count=1 --mixing_t_ood=0.4 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+python notebooks/evaluate_notebook.py --ckpt=2502/BioSR-joint_indi-l1/5 --mmse_count=1 --mixing_t_ood=0.5 --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateInputvsTarget.ipynb
+

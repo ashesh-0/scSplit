ce3df7917a4c900d93bfd7ea0e53563e145bb5d0
Ashesh <ashesh276@gmail.com>
2025-03-03 09:54:21 +0100

    evaluate jointindi fix

---
+++ notebooks/hagen.sh
@@ -0,0 +1,32 @@
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/63 --mixing_t_ood=0.1 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/63 --mixing_t_ood=0.2 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/63 --mixing_t_ood=0.3 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/63 --mixing_t_ood=0.4 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/63 --mixing_t_ood=0.5 --use_hardcoded_time_for_inference=0.5
+
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/61 --mixing_t_ood=0.1 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/61 --mixing_t_ood=0.2 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/61 --mixing_t_ood=0.3 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/61 --mixing_t_ood=0.4 --use_hardcoded_time_for_inference=0.5
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/Hagen-joint_indi-l1/61 --mixing_t_ood=0.5 --use_hardcoded_time_for_inference=0.5
+
+
+
+
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.1 --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.2 --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.3 --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.4 --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.5 --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+
+
+
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.1 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.2 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.3 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.4 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+python notebooks/evaluate_notebook.py  --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndi.ipynb --ckpt=2502/HT_T24-joint_indi-l1/63 --mixing_t_ood=0.5 --use_aggregated_inferred_time=true --infer_time=true --ckpt_time_predictor=2502/Hagen-UnetClassifier-l2/13
+
+
+# real input
+python /home/ashesh.ashesh/code/DiffSplitting/notebooks/evaluate_notebook.py --ckpt=2502/Hagen-joint_indi-l1/63 --mmse_count=10  --notebook=/home/ashesh.ashesh/code/DiffSplitting/notebooks/EvaluateJointIndiRealInput.ipynb  --use_hardcoded_time_for_inference=0.5

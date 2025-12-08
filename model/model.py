4303212b2fd3ed0599573e36b87e28cde491e112
Ashesh <ashesh276@gmail.com>
2025-12-08 02:49:39 +0100

    a model which can work with a given input

--- model/model.py
+++ model/model.py
@@ -6,18 +6,22 @@ import torch.nn as nn
 import os
 import model.networks as networks
 from .base_model import BaseModel
-logger = logging.getLogger('base')
+
+logger = logging.getLogger("base")
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+
 def print_parameter_count(model):
     count = count_parameters(model)
-    print('')
-    print('--------------------------------------------------')
-    print(f'Number of trainable parameters: {count/1e6:.2f}M')
-    print('--------------------------------------------------')
-    print('')
+    print("")
+    print("--------------------------------------------------")
+    print(f"Number of trainable parameters: {count / 1e6:.2f}M")
+    print("--------------------------------------------------")
+    print("")
+
 
 class DDPM(BaseModel):
     def __init__(self, opt):
@@ -28,37 +32,32 @@ class DDPM(BaseModel):
         self.best_val_metric = None
         # set loss and load resume state
         self.set_loss()
-        self.set_new_noise_schedule(
-            opt['model']['beta_schedule']['train'], schedule_phase='train')
-        if self.opt['phase'] == 'train':
+        self.set_new_noise_schedule(opt["model"]["beta_schedule"]["train"], schedule_phase="train")
+        if self.opt["phase"] == "train":
             self.netG.train()
             # find the parameters to optimize
-            if opt['model']['finetune_norm']:
+            if opt["model"]["finetune_norm"]:
                 optim_params = []
                 for k, v in self.netG.named_parameters():
                     v.requires_grad = False
-                    if k.find('transformer') >= 0:
+                    if k.find("transformer") >= 0:
                         v.requires_grad = True
                         v.data.zero_()
                         optim_params.append(v)
-                        logger.info(
-                            'Params [{:s}] initialized to 0 and will optimize.'.format(k))
+                        logger.info("Params [{:s}] initialized to 0 and will optimize.".format(k))
             else:
                 optim_params = list(self.netG.parameters())
 
-            if opt['train']['optimizer']['type'] == 'adam':
-                self.optG = torch.optim.Adam(optim_params, lr=opt['train']["optimizer"]["lr"])
-            elif opt['train']['optimizer']['type'] == 'adamax':
-                self.optG = torch.optim.Adamax(optim_params, lr=opt['train']["optimizer"]["lr"], weight_decay=0)
-            
+            if opt["train"]["optimizer"]["type"] == "adam":
+                self.optG = torch.optim.Adam(optim_params, lr=opt["train"]["optimizer"]["lr"])
+            elif opt["train"]["optimizer"]["type"] == "adamax":
+                self.optG = torch.optim.Adamax(optim_params, lr=opt["train"]["optimizer"]["lr"], weight_decay=0)
+
             # set scheduler on PSNR, and therefore, it is max.
-            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optG,
-                                                         'max',
-                                                         patience=opt['train']['lr_scheduler']['patience'],
-                                                         factor=0.2,
-                                                         min_lr=1e-5,
-                                                         verbose=True)
-            print('Scheduler set to ReduceLROnPlateau with patience: ', opt['train']['lr_scheduler']['patience'])
+            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                self.optG, "max", patience=opt["train"]["lr_scheduler"]["patience"], factor=0.2, min_lr=1e-5
+            )
+            print("Scheduler set to ReduceLROnPlateau with patience: ", opt["train"]["lr_scheduler"]["patience"])
             self.log_dict = OrderedDict()
         self.load_network()
         print_parameter_count(self.netG)
@@ -77,7 +76,7 @@ class DDPM(BaseModel):
         self.optG.step()
 
         # set log
-        self.log_dict['l_pix'] = l_pix.item()
+        self.log_dict["l_pix"] = l_pix.item()
         diffusion_log = self.netG.get_current_log()
         for k, v in diffusion_log.items():
             self.log_dict[k] = v
@@ -90,21 +89,26 @@ class DDPM(BaseModel):
         with torch.no_grad():
             if isinstance(self.netG, nn.DataParallel):
                 self.prediction = self.netG.module.inference(
-                    self.data['input'], 
+                    self.data["input"],
                     # clip_denoised=clip_denoised,
-                    continuous=continuous)
+                    continuous=continuous,
+                )
             else:
-                if len(self.data['input'].shape) ==1:
-                    ch1 = self.data['target'][:,:1]
-                    ch2 = self.data['target'][:,1:]
-                    inp1, inp2 = self.netG.get_xt_clean(ch1, ch2, torch.Tensor([0.5]*len(ch1)).to(ch1.device), update=True)
+                if len(self.data["input"].shape) == 1:
+                    ch1 = self.data["target"][:, :1]
+                    ch2 = self.data["target"][:, 1:]
+                    inp1, inp2 = self.netG.get_xt_clean(
+                        ch1, ch2, torch.Tensor([0.5] * len(ch1)).to(ch1.device), update=True
+                    )
                 else:
-                    ch1 = self.data['target']
-                    ch2 = self.data['input']
-                    inp1 = self.netG.get_xt_clean(ch1, ch2, torch.Tensor([0.999]*len(ch1)).to(ch1.device), update=True)
+                    ch1 = self.data["target"]
+                    ch2 = self.data["input"]
+                    inp1 = self.netG.get_xt_clean(
+                        ch1, ch2, torch.Tensor([0.999] * len(ch1)).to(ch1.device), update=True
+                    )
                 # the hope is that with time, once the statistics are learned, both inp1 and inp2 will be the same.
-                # normalization happens here. 
-                self.prediction = self.netG.inference(inp1,continuous=continuous)
+                # normalization happens here.
+                self.prediction = self.netG.inference(inp1, continuous=continuous)
 
         self.netG.train()
 
@@ -123,12 +127,11 @@ class DDPM(BaseModel):
         else:
             self.netG.set_loss(self.device)
 
-    def set_new_noise_schedule(self, schedule_opt, schedule_phase='train'):
+    def set_new_noise_schedule(self, schedule_opt, schedule_phase="train"):
         if self.schedule_phase is None or self.schedule_phase != schedule_phase:
             self.schedule_phase = schedule_phase
             if isinstance(self.netG, nn.DataParallel):
-                self.netG.module.set_new_noise_schedule(
-                    schedule_opt, self.device)
+                self.netG.module.set_new_noise_schedule(schedule_opt, self.device)
             else:
                 self.netG.set_new_noise_schedule(schedule_opt, self.device)
 
@@ -138,11 +141,11 @@ class DDPM(BaseModel):
     def get_current_visuals(self, need_LR=True, sample=False):
         out_dict = OrderedDict()
         if sample:
-            out_dict['SAM'] = self.prediction.detach().float().cpu()
+            out_dict["SAM"] = self.prediction.detach().float().cpu()
         else:
-            out_dict['prediction'] = self.prediction.detach().float().cpu()
-            out_dict['input'] = self.data['input'].detach().float().cpu()
-            out_dict['target'] = self.data['target'].detach().float().cpu()
+            out_dict["prediction"] = self.prediction.detach().float().cpu()
+            out_dict["input"] = self.data["input"].detach().float().cpu()
+            out_dict["target"] = self.data["target"].detach().float().cpu()
             # if need_LR and 'LR' in self.data:
             #     out_dict['LR'] = self.data['LR'].detach().float().cpu()
             # else:
@@ -152,13 +155,11 @@ class DDPM(BaseModel):
     def print_network(self):
         s, n = self.get_network_description(self.netG)
         if isinstance(self.netG, nn.DataParallel):
-            net_struc_str = '{} - {}'.format(self.netG.__class__.__name__,
-                                             self.netG.module.__class__.__name__)
+            net_struc_str = "{} - {}".format(self.netG.__class__.__name__, self.netG.module.__class__.__name__)
         else:
-            net_struc_str = '{}'.format(self.netG.__class__.__name__)
+            net_struc_str = "{}".format(self.netG.__class__.__name__)
 
-        logger.info(
-            'Network G structure: {}, with parameters: {:,d}'.format(net_struc_str, n))
+        logger.info("Network G structure: {}, with parameters: {:,d}".format(net_struc_str, n))
         logger.info(s)
 
     def save_best_network(self, epoch, iter_step, metric_val):
@@ -167,8 +168,7 @@ class DDPM(BaseModel):
         else:
             return False
 
-        opt_path = os.path.join(
-            self.opt['path']['checkpoint'], 'I{}_E{}_opt.pth'.format(iter_step, epoch))
+        opt_path = os.path.join(self.opt["path"]["checkpoint"], "I{}_E{}_opt.pth".format(iter_step, epoch))
         # gen
         network = self.netG
         if isinstance(self.netG, nn.DataParallel):
@@ -176,30 +176,27 @@ class DDPM(BaseModel):
         state_dict = network.state_dict()
         for key, param in state_dict.items():
             state_dict[key] = param.cpu()
-  
+
         # opt
-        opt_state = {'epoch': epoch, 'iter': iter_step,
-                     'scheduler': None, 'optimizer': None}
-        opt_state['optimizer'] = self.optG.state_dict()
+        opt_state = {"epoch": epoch, "iter": iter_step, "scheduler": None, "optimizer": None}
+        opt_state["optimizer"] = self.optG.state_dict()
         torch.save(opt_state, opt_path)
 
         # delete the previous best model
-        for f in os.listdir(self.opt['path']['checkpoint']):
-            if f.startswith('best_gen'):
-                os.remove(os.path.join(self.opt['path']['checkpoint'], f))
+        for f in os.listdir(self.opt["path"]["checkpoint"]):
+            if f.startswith("best_gen"):
+                os.remove(os.path.join(self.opt["path"]["checkpoint"], f))
                 break
-            
-        best_path = os.path.join(self.opt['path']['checkpoint'], 'best_gen_I{}_E{}_gen.pth.pth'.format(iter_step, epoch))
+
+        best_path = os.path.join(
+            self.opt["path"]["checkpoint"], "best_gen_I{}_E{}_gen.pth.pth".format(iter_step, epoch)
+        )
         torch.save(state_dict, best_path)
-        logger.info('Saved best model in [{:s}] ...'.format(best_path))
-      
+        logger.info("Saved best model in [{:s}] ...".format(best_path))
 
     def save_network(self, epoch, iter_step):
-        
-        gen_path = os.path.join(
-            self.opt['path']['checkpoint'], 'I{}_E{}_gen.pth'.format(iter_step, epoch))
-        opt_path = os.path.join(
-            self.opt['path']['checkpoint'], 'I{}_E{}_opt.pth'.format(iter_step, epoch))
+        gen_path = os.path.join(self.opt["path"]["checkpoint"], "I{}_E{}_gen.pth".format(iter_step, epoch))
+        opt_path = os.path.join(self.opt["path"]["checkpoint"], "I{}_E{}_opt.pth".format(iter_step, epoch))
         # gen
         network = self.netG
         if isinstance(self.netG, nn.DataParallel):
@@ -208,34 +205,30 @@ class DDPM(BaseModel):
         for key, param in state_dict.items():
             state_dict[key] = param.cpu()
         torch.save(state_dict, gen_path)
-  
+
         # opt
-        opt_state = {'epoch': epoch, 'iter': iter_step,
-                     'scheduler': None, 'optimizer': None}
-        opt_state['optimizer'] = self.optG.state_dict()
+        opt_state = {"epoch": epoch, "iter": iter_step, "scheduler": None, "optimizer": None}
+        opt_state["optimizer"] = self.optG.state_dict()
         torch.save(opt_state, opt_path)
 
-        logger.info(
-            'Saved model in [{:s}] ...'.format(gen_path))
+        logger.info("Saved model in [{:s}] ...".format(gen_path))
 
     def load_network(self):
-        load_path = self.opt['path']['resume_state']
+        load_path = self.opt["path"]["resume_state"]
         if load_path is not None:
-            logger.info(
-                'Loading pretrained model for G [{:s}] ...'.format(load_path))
-            gen_path = '{}_gen.pth'.format(load_path)
-            opt_path = '{}_opt.pth'.format(load_path)
+            logger.info("Loading pretrained model for G [{:s}] ...".format(load_path))
+            gen_path = "{}_gen.pth".format(load_path)
+            opt_path = "{}_opt.pth".format(load_path)
             # gen
             network = self.netG
             if isinstance(self.netG, nn.DataParallel):
                 network = network.module
             # network.load_state_dict(torch.load(
             #     gen_path), strict=(not self.opt['model']['finetune_norm']))
-            network.load_state_dict(torch.load(
-                gen_path), strict=False)
-            if self.opt['phase'] == 'train':
+            network.load_state_dict(torch.load(gen_path), strict=False)
+            if self.opt["phase"] == "train":
                 # optimizer
                 opt = torch.load(opt_path)
-                self.optG.load_state_dict(opt['optimizer'])
-                self.begin_step = opt['iter']
-                self.begin_epoch = opt['epoch']
+                self.optG.load_state_dict(opt["optimizer"])
+                self.begin_step = opt["iter"]
+                self.begin_epoch = opt["epoch"]
--- model/normalizer.py
+++ model/normalizer.py
@@ -2,47 +2,60 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 class NormalizerXT(nn.Module):
     """
     A class which returns the normalization parameters for x_t.
     """
-    def __init__(self, data_mean=None, data_std=None, data_count=None, num_bins=100, stop_update_count=1e6):
+
+    def __init__(
+        self, data_mean=None, data_std=None, data_count=None, num_bins=100, stop_update_count=1e6, device="cpu"
+    ):
         super().__init__()
         self.data_mean_fixed = data_mean
         self.data_std_fixed = data_std
         self.num_bins = num_bins
         self.stop_update_count = stop_update_count
         if self.data_mean_fixed is None:
-            self.register_buffer("data_mean",torch.Tensor([0.0]*num_bins).cuda())
-            self.register_buffer("data_std",torch.Tensor([1.0]*num_bins).cuda())
-            self.register_buffer("count",torch.Tensor([0.0]*num_bins))
+            self.register_buffer("data_mean", torch.Tensor([0.0] * num_bins).to(device))
+            self.register_buffer("data_std", torch.Tensor([1.0] * num_bins).to(device))
+            self.register_buffer("count", torch.Tensor([0.0] * num_bins))
             assert self.data_std_fixed is None
-            self.count = torch.Tensor([0.0]*num_bins)
+            self.count = torch.Tensor([0.0] * num_bins)
         else:
-            self.register_buffer("data_mean",torch.Tensor(self.data_mean_fixed).cuda())
-            self.register_buffer("data_std",torch.Tensor(self.data_std_fixed).cuda())
-            self.register_buffer("count",torch.Tensor([data_count]*num_bins))
+            self.register_buffer("data_mean", torch.Tensor(self.data_mean_fixed).to(device))
+            self.register_buffer("data_std", torch.Tensor(self.data_std_fixed).to(device))
+            self.register_buffer("count", torch.Tensor([data_count] * num_bins))
+
+    def set_device(self, device):
+        self.data_mean = self.data_mean.to(device)
+        self.data_std = self.data_std.to(device)
+        self.count = self.count.to(device)
 
     def update(self, x_t, t):
         # assert self.data_mean_fixed is None, "update() should not be called when data_mean is fixed."
 
         for batch_idx in range(x_t.shape[0]):
-            t_bin = int(np.floor(t[batch_idx].item()*self.num_bins))
+            t_bin = int(np.floor(t[batch_idx].item() * self.num_bins))
             if t_bin == self.num_bins:
                 # print("Warning: t_bin is at the last bin. for t:", t[batch_idx].item())
                 t_bin = self.num_bins - 1
-            
-            self.data_mean[t_bin] = (self.data_mean[t_bin]*self.count[t_bin] + x_t[batch_idx].mean())/(1 + self.count[t_bin])
-            self.data_std[t_bin] = (self.data_std[t_bin]*self.count[t_bin] + x_t[batch_idx].std())/(1 + self.count[t_bin])
+
+            self.data_mean[t_bin] = (self.data_mean[t_bin] * self.count[t_bin] + x_t[batch_idx].mean()) / (
+                1 + self.count[t_bin]
+            )
+            self.data_std[t_bin] = (self.data_std[t_bin] * self.count[t_bin] + x_t[batch_idx].std()) / (
+                1 + self.count[t_bin]
+            )
             self.count[t_bin] += 1
-    
+
     def normalize(self, x_t, t, update=False):
         if update and torch.sum(self.count) < self.stop_update_count:
             self.update(x_t, t)
-        
-        param_shape = [len(x_t)] + [1]*(len(x_t.shape)-1)
+
+        param_shape = [len(x_t)] + [1] * (len(x_t.shape) - 1)
         t_bins = (t * self.num_bins).type(torch.long)
         t_bins[t_bins == self.num_bins] = self.num_bins - 1
-        mean_val = self.data_mean[t_bins].reshape(param_shape)
-        std_val = self.data_std[t_bins].reshape(param_shape)
+        mean_val = self.data_mean[t_bins].reshape(param_shape).to(x_t.device)
+        std_val = self.data_std[t_bins].reshape(param_shape).to(x_t.device)
         return (x_t - mean_val) / std_val
